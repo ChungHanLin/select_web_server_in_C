57816199e435f60c0137e4d9a8947a6f73970094
lcha105u <lcha105u@gmail.com>
2018-11-20 02:46:05 +0800

    more clean

--- client.c
+++ client.c
@@ -1,45 +1,65 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <unistd.h>
 #include <arpa/inet.h>
+#include "socket.h"
+
+#define PORT_NUM 8000
+#define BUFFER_SIZE 4096
+
+void init_Client(int, char **, char *);
+void connect_Socket(int , struct sockaddr_in *);
+void socket_Handler(int, char *, int);
 
 int main(int argc, char *argv[]){
-	int network_socket;	// socket fd
-	char *address;
+	int clientSocket;	// socket fd
+	char socketAddress[128];
+	char response[] = "GET / HTTP/1.1\r\n\r\n";
+	int messageLength = strlen(response);
+	struct sockaddr_in clientInfo;
 
-	address = argv[1];
+	init_Client(argc, argv, socketAddress);
 
-	network_socket = socket(AF_INET, SOCK_STREAM, 0);	//create a socket
-	if(network_socket == -1){
-		perror("socket error");
-	}
-	
-	struct sockaddr_in s_address;
-	s_address.sin_family = AF_INET;				// IPV4
-	s_address.sin_port = htons(8099);				// spec asks set port:80
-	s_address.sin_addr.s_addr = inet_addr("127.0.0.1");			// connect to local machine (able to connect real IP)
-	inet_aton(address, (struct in_addr *) &s_address.sin_addr.s_addr);
+	clientSocket = getSocketFD();
 
-	int connect_status = connect(network_socket, (struct sockaddr *) &s_address, sizeof(s_address));
+	init_Socket(&clientInfo, socketAddress);
 
-	if(connect_status == -1){
-		perror("connection failure");
-	}
+	connect_Socket(clientSocket, &clientInfo);
 
-	char request[] = "GET / HTTP/1.1\r\n\r\n";
-	char response[4096];
+	socket_Handler(clientSocket, response, messageLength);
 
-	// send message to socket
-	send(network_socket, request, sizeof(request), 0);
-
-	// recieve message from socket
-	recv(network_socket, response, sizeof(response), 0);
-	
-	printf("response from the server : \n%s\n", response);
-	close(network_socket);
+	close(clientSocket);
 
 	return 0;
+}
+
+void init_Client(int argc, char **argv, char *socketAddress){
+	if(argc != 2){
+		fprintf(stderr, "Not enough argument\n");
+		exit(EXIT_FAILURE);
+	}
+	strcpy(socketAddress,argv[1]);
+}
+
+void connect_Socket(int socketFD, struct sockaddr_in *socketInfo){
+	
+	if((connect(socketFD, (struct sockaddr *)socketInfo, sizeof(struct sockaddr_in))) == -1){
+		perror("connect");
+		close(socketFD);
+		exit(EXIT_FAILURE);
+	}
+}
+
+void socket_Handler(int socketFD, char *response, int messageLength){
+	
+	char recieve[BUFFER_SIZE];
+
+	send(socketFD, response, messageLength, 0);
+	recv(socketFD, recieve, BUFFER_SIZE, 0);
+	printf("Recieve:\n%s\n", recieve);
+	return;
 }
