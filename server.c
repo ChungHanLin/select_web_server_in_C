36855c501b9160881b740f0f22c85e8867a07d7c
lcha105u <lcha105u@gmail.com>
2018-11-20 02:46:51 +0800

    Add files via upload

--- server.c
+++ server.c
@@ -1,109 +1,84 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+#include <signal.h>
+#include <time.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <sys/stat.h>
+#include <sys/wait.h>
 #include <fcntl.h>
 #include <netinet/in.h>
-#include <unistd.h>
-#include <sys/select.h>
-#include <time.h>
+#include "socket.h"
 
-#define Port_Num 8099
-#define Background 10
+#define PORT_NUM 8000
+#define BUFFER_SIZE 4096
+#define BACKLOG_NUM 5
 
-void write_protocol(char *http_header);
+
+void write_Protocol(char *);
 char *get_time(char *);
+int get_Request(char *response);
 
-void socket_handler(int socket_fd, char *recieve, char *response, int length, fd_set *set){
-	send(socket_fd, response, length, 0);
-	recv(socket_fd, recieve, 1024, 0);
-	close(socket_fd);
-	FD_CLR(socket_fd, set);
+void bind_Server(int, struct sockaddr_in *);
+void listen_Server(int);
+void signal_Action(int ,struct sigaction *);
+int accept_Client(int, struct sockaddr_in *);
+void socket_Handler(int, char *, int);
+
+static void wait_child(int singNum){
+	while(waitpid(-1, NULL, WNOHANG) > 0);
 }
 
 int main(int argc, char *argv[]){
-	
-	//open html file
-	int htmlFD = open("index.html", O_RDONLY);
-	char htmlContent[4096];
-	char recieve[1024];
-	int length = read(htmlFD, htmlContent, 4096);
-	
-	htmlContent[length] = '\0';
-
-	char response[2048];
-	write_protocol(response);
-
-	length += strlen(response);
-	strcat(response, htmlContent);
-
-	int network_socket, new_socket, maxsock;
-
-	network_socket = socket(AF_INET, SOCK_STREAM, 0);
-
-	if(network_socket == -1){
-		perror("socket error");
-	}
-
+	int serverSocket, clientSocket;
+	int replyLength = 0;
+	char recieve[BUFFER_SIZE], response[BUFFER_SIZE];
 	struct sockaddr_in serverInfo, clientInfo;
-	int client_addrlen = sizeof(clientInfo);
-	fd_set socks, readsocks;
+	struct sigaction s;
 
-	serverInfo.sin_family = AF_INET;
-	serverInfo.sin_addr.s_addr = INADDR_ANY;
-	serverInfo.sin_port = htons(Port_Num);
+	serverSocket = getSocketFD();
 
-	if(bind(network_socket, (struct sockaddr *) &serverInfo, sizeof(serverInfo)) == -1){
-		perror("bind error");
-	}
+	replyLength = get_Request(response);
+	
+	init_Socket(&serverInfo, NULL);
 
-	if(listen(network_socket, Background) == -1){
-		perror("listen error");
-	}
+	bind_Server(serverSocket, &serverInfo);
 
-	FD_ZERO(&socks);
-	FD_SET(network_socket, &socks);
-	maxsock = network_socket;
+	listen_Server(serverSocket);
+
+	signal_Action(serverSocket, &s);
 
 	while(1){
-		unsigned int s;
-		readsocks = socks;
-		if(select(maxsock + 1, &readsocks, NULL, NULL, NULL) == -1){
-			perror("select");
+		int pid;
+
+		clientSocket = accept_Client(serverSocket, &clientInfo);
+
+		fprintf(stderr, "execute\n");
+
+		pid = fork();
+
+		if(pid < 0){
+			perror("fork error");
+			close(clientSocket);
+			close(serverSocket);
+			exit(EXIT_FAILURE);
 		}
-		
-		for(s = 0; s <= maxsock; s++){
-			if(FD_ISSET(s, &readsocks)){
-				printf("socket %d was ready\n", s);
-				if(s == network_socket){
-					// New Connection
-					new_socket = accept(network_socket, (struct sockaddr *) &clientInfo, (unsigned int *) &client_addrlen);
-					if(new_socket == -1){
-						perror("client_socket error");
-						exit(EXIT_FAILURE);
-					}
-					else{
-						FD_SET(new_socket, &socks);
-						if(new_socket > maxsock){
-							maxsock = new_socket;
-						}
-					}
-				}
-				else{
-					socket_handler((unsigned int)s, recieve, response, length, &socks);
-				}
-			}
+
+		else if(pid == 0){
+			socket_Handler(clientSocket, response, replyLength);
 		}
+
+		close(clientSocket);
 	}
 
-	close(network_socket);
-	printf("done\n");
+	close(serverSocket);
 	return 0;
 }
 
 char *get_time(char *c_time_string){
+
 	time_t current_time;
 
 	current_time = time(NULL);
@@ -123,16 +98,81 @@ char *get_time(char *c_time_string){
 	return c_time_string;
 }
 
-void write_protocol(char *http_header){
+void write_Protocol(char *http_header){
 	char header[] = "HTTP/1.1 200 OK\r\n";
 	char host[] = "Host:localhost\r\n";
-	char port[] = "Port:8001\r\n";
+	char port[] = "Port:";
 	char time[512];
 	char *time_Reg = get_time(time);
 
-	strcat(http_header, header);
-	strcat(http_header, host);
-	//strcat(http_header, Port_Num);
-	strcat(http_header, time_Reg);
-	strcat(http_header, "\r\n\n");
+	sprintf(http_header, "%s%s%s%d\r\n%s\r\n\n", header, host, port, PORT_NUM, time_Reg);
+}
+
+int get_Request(char *response){
+	int htmlFD = open("index.html", O_RDONLY);
+	char htmlContent[BUFFER_SIZE];
+	int length = read(htmlFD, htmlContent, BUFFER_SIZE);
+
+	htmlContent[length] = '\0';
+	write_Protocol(response);
+
+	length += strlen(response);
+	strcat(response, htmlContent);
+
+	return length;
+}
+
+void bind_Server(int socketFD, struct sockaddr_in *serverInfo){
+
+	if(bind(socketFD, (struct sockaddr *) serverInfo, sizeof(struct sockaddr_in)) == -1){
+		perror("bind");
+		close(socketFD);
+		exit(EXIT_FAILURE);
+	}
+}
+
+void listen_Server(int socketFD){
+
+	if(listen(socketFD, BACKLOG_NUM) == -1){
+		perror("listen");
+		close(socketFD);
+		exit(EXIT_FAILURE);
+	}
+}
+
+void signal_Action(int socketFD, struct sigaction *s){
+	
+	s->sa_handler = wait_child;
+	sigemptyset(& (s->sa_mask));
+	s->sa_flags = SA_RESTART;
+
+	if(sigaction(SIGCHLD, s, NULL) == -1){
+		perror("sigaction");
+		close(socketFD);
+		exit(EXIT_FAILURE);
+	}
+}
+
+int accept_Client(int serverSocketFD, struct sockaddr_in *clientInfo){
+	
+	int clientSocketFD;
+	socklen_t length = sizeof(struct sockaddr_in);
+
+	clientSocketFD = accept(serverSocketFD, (struct sockaddr *) clientInfo, &length);
+
+	if(clientSocketFD == -1){
+		perror("accept");
+		close(serverSocketFD);
+		exit(EXIT_FAILURE);
+	}
+}
+
+void socket_Handler(int socketFD, char *response, int messageLength){
+	
+	char recieve[BUFFER_SIZE];
+
+	recv(socketFD, recieve, BUFFER_SIZE, 0);
+	send(socketFD, response, messageLength, 0);
+	printf("Recieve:\n%s\n", recieve);
+	return;
 }
