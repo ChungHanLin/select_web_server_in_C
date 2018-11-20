162675485509795cf39633c3a9ea0cacfa427fee
lcha105u <lcha105u@gmail.com>
2018-11-21 01:19:17 +0800

    much more statistic

--- server.c
+++ server.c
@@ -1,76 +1,78 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
-#include <signal.h>
-#include <time.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <sys/stat.h>
-#include <sys/wait.h>
 #include <fcntl.h>
 #include <netinet/in.h>
+#include <unistd.h>
+#include <sys/select.h>
+#include <time.h>
 #include "socket.h"
 
 #define PORT_NUM 8000
 #define BUFFER_SIZE 4096
-#define BACKLOG_NUM 5
+#define BACKLOG_NUM 10
 
-
-void write_Protocol(char *);
+void write_Protocol(char *http_header);
 char *get_time(char *);
 int get_Request(char *response);
 
 void bind_Server(int, struct sockaddr_in *);
 void listen_Server(int);
-void signal_Action(int ,struct sigaction *);
+
+void init_Selector(int, fd_set *);
+void select_Client(int, fd_set *);
 int accept_Client(int, struct sockaddr_in *);
 void socket_Handler(int, char *, int);
-
-static void wait_child(int singNum){
-	while(waitpid(-1, NULL, WNOHANG) > 0);
-}
+void add_SocketFD(int, fd_set *, int *, int *);
+void clear_SocketFD(int, fd_set *, int *, int *);
 
 int main(int argc, char *argv[]){
 	int serverSocket, clientSocket;
+	int minSocket, maxSocket;
 	int replyLength = 0;
-	char recieve[BUFFER_SIZE], response[BUFFER_SIZE];
+	char response[BUFFER_SIZE];
 	struct sockaddr_in serverInfo, clientInfo;
-	struct sigaction s;
+
+	fd_set initFDSet, readFDSet;
 
 	serverSocket = getSocketFD();
 
 	replyLength = get_Request(response);
-	
+
 	init_Socket(&serverInfo, NULL);
 
 	bind_Server(serverSocket, &serverInfo);
 
 	listen_Server(serverSocket);
 
-	signal_Action(serverSocket, &s);
+	init_Selector(serverSocket, &initFDSet);
+
+	minSocket = maxSocket = serverSocket;
 
 	while(1){
-		int pid;
+		int s;
 
-		clientSocket = accept_Client(serverSocket, &clientInfo);
+		readFDSet = initFDSet;
 
-		fprintf(stderr, "execute\n");
+		select_Client(maxSocket, &readFDSet);
 
-		pid = fork();
+		for(s = minSocket; s <= maxSocket; s++){
 
-		if(pid < 0){
-			perror("fork error");
-			close(clientSocket);
-			close(serverSocket);
-			exit(EXIT_FAILURE);
+			if(FD_ISSET(s, &readFDSet)){
+				if(s == serverSocket){
+					clientSocket = accept_Client(serverSocket, &clientInfo);
+					add_SocketFD(clientSocket, &initFDSet, &minSocket, &maxSocket);
+				}
+				else{
+					socket_Handler(s, response, replyLength);
+					clear_SocketFD(s, &initFDSet, &minSocket, &maxSocket);
+					close(s);
+				}
+			}
 		}
-
-		else if(pid == 0){
-			socket_Handler(clientSocket, response, replyLength);
-		}
-
-		close(clientSocket);
 	}
 
 	close(serverSocket);
@@ -78,23 +80,22 @@ int main(int argc, char *argv[]){
 }
 
 char *get_time(char *c_time_string){
-
 	time_t current_time;
 
 	current_time = time(NULL);
 
 	if (current_time == ((time_t)-1)){
-        	fprintf(stderr, "Failure to obtain the current time.\n");
+		fprintf(stderr, "Failure to obtain the current time.\n");
 		exit(EXIT_FAILURE);    	
 	}
 
-   	/* Convert to local time format. */
-    	c_time_string = ctime(&current_time);
+	/* Convert to local time format. */
+	c_time_string = ctime(&current_time);
 
-    	if (c_time_string == NULL){
-        	fprintf(stderr, "Failure to convert the current time.\n");
-        	exit(EXIT_FAILURE);
-    	}
+	if (c_time_string == NULL){
+		fprintf(stderr, "Failure to convert the current time.\n");
+		exit(EXIT_FAILURE);
+	}
 	return c_time_string;
 }
 
@@ -140,39 +141,71 @@ void listen_Server(int socketFD){
 	}
 }
 
-void signal_Action(int socketFD, struct sigaction *s){
-	
-	s->sa_handler = wait_child;
-	sigemptyset(& (s->sa_mask));
-	s->sa_flags = SA_RESTART;
 
-	if(sigaction(SIGCHLD, s, NULL) == -1){
-		perror("sigaction");
-		close(socketFD);
+void init_Selector(int socketFD, fd_set *initFDSet){
+
+	FD_ZERO(initFDSet);
+	FD_SET(socketFD, initFDSet);
+}
+
+void select_Client(int socketFD, fd_set *readFDSet){
+
+	if(select(socketFD + 1, readFDSet, NULL, NULL, NULL) == -1){
+		perror("select");
 		exit(EXIT_FAILURE);
 	}
 }
 
-int accept_Client(int serverSocketFD, struct sockaddr_in *clientInfo){
-	
-	int clientSocketFD;
+int accept_Client(int serverSocket, struct sockaddr_in *clientInfo){
 	socklen_t length = sizeof(struct sockaddr_in);
 
-	clientSocketFD = accept(serverSocketFD, (struct sockaddr *) clientInfo, &length);
+	int clientSocket = accept(serverSocket, (struct sockaddr *)clientInfo, &length);
 
-	if(clientSocketFD == -1){
+	if(clientSocket == -1){
 		perror("accept");
-		close(serverSocketFD);
 		exit(EXIT_FAILURE);
 	}
+	return clientSocket;
 }
 
 void socket_Handler(int socketFD, char *response, int messageLength){
-	
+
 	char recieve[BUFFER_SIZE];
 
 	recv(socketFD, recieve, BUFFER_SIZE, 0);
 	send(socketFD, response, messageLength, 0);
-	printf("Recieve:\n%s\n", recieve);
+
 	return;
+}
+
+void add_SocketFD(int index, fd_set *initFDSet, int *minSocket, int *maxSocket){
+
+	FD_SET(index, initFDSet);
+	
+	if(index > *maxSocket){
+		*maxSocket = index;
+	}
+	else if(index < *minSocket){
+		*minSocket = index;
+	}
+
+}
+
+void clear_SocketFD(int index, fd_set *initFDSet, int *minSocket, int *maxSocket){
+	int replaceIndex = index;
+
+	FD_CLR(index, initFDSet);
+
+	if(index == *maxSocket){
+		while((replaceIndex > *minSocket) && (!FD_ISSET(replaceIndex, initFDSet))){
+			replaceIndex--;
+		}
+		*maxSocket = replaceIndex;
+	}
+	else if(index == *minSocket){
+		while((replaceIndex < *maxSocket) && (!FD_ISSET(replaceIndex, initFDSet))){
+			replaceIndex++;
+		}
+		*minSocket = replaceIndex;
+	}
 }
